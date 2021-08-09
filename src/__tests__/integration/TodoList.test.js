0bc936d390675082806465772416d0f9132434ae
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-08-09 11:15:11 +0200

    Add more acceptance test for TodoList.js

--- src/__tests__/integration/TodoList.test.js
+++ src/__tests__/integration/TodoList.test.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 jest.mock('axios');
 
 describe('TodoList acceptance', () => {
-  test('fetches todoList from API and renders it', async () => {
+  test('Should fetch todoList from API and renders it', async () => {
     const todoList = [
       {
         userId: 1,
@@ -31,5 +31,45 @@ describe('TodoList acceptance', () => {
 
     const items = await screen.findAllByRole('listitem');
     expect(items).toHaveLength(2);
+  });
+
+  test('should have todo title displayed on the page', async () => {
+    const todoList = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autem',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false,
+      },
+    ];
+
+    axios.get.mockImplementationOnce(() => {
+      Promise.resolve({
+        data: todoList,
+      });
+    });
+
+    render(<TodoList />);
+    const title1 = await screen.findByText('delectus aut autem');
+    expect(title1).toBeInTheDocument();
+    const title2 = await await screen.findByText(
+      'quis ut nam facilis et officia qui'
+    );
+    expect(title2).toBeInTheDocument();
+  });
+
+  test('Should display error message when fetch request fails', async () => {
+    axios.get.mockImplementationOnce(() => {
+      Promise.reject(new Error());
+    });
+    render(<TodoList />);
+    const error = await screen.findByText(/something went wrong/i);
+    expect(error).toBeInTheDocument();
   });
 });
---
+++ src/__tests__/unit/TodoList.test.js
@@ -0,0 +1,4 @@
+import {screen render} from '@testing-library/react';
+import TodoList from '../../components/TodoList';
+
+describe('')
